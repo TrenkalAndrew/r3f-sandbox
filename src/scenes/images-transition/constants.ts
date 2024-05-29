6c06f042e359b6f6aa539653e801ae0c2b402412
Andrew Trenkal <trenkal.andrey@mail.ru>
2024-05-30 00:54:34 +0300

    changed urls for images

--- src/scenes/images-transition/constants.ts
+++ src/scenes/images-transition/constants.ts
@@ -1,3 +1,6 @@
-export const IMAGE_URIS = ["/images/cat1.jpeg", "/images/cat2.jpeg"];
+import cat1 from '/images/cat1.jpeg';
+import cat2 from '/images/cat2.jpeg';
+
+export const IMAGE_URIS = [cat1, cat2];
 
 export const ANIMATION_DURATION_MS = 3000;
